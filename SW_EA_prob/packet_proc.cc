dac12577556319ea3019945a7c5b86ec463be387
UserHK <UserHK@DESKTOP-LJTC6O6>
2023-01-10 12:28:43 +0900

    packet solution update

--- SW_EA_prob/packet_proc.cc
+++ SW_EA_prob/packet_proc.cc
@@ -1,4 +1,4 @@
-#include "/home/icegpu/HK/HKTool.h"
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,74 +13,47 @@ int queSum[5];
 
 int mx = 0;
 
-void run(int start, int cnt){
+void run(int nCPU, int prev, int cnt){
 	if(cnt == n){
+		mx = nCPU;
 		return;
 	}
-
+	
 	pair<int,int> p = vp[cnt];
-	for(int i = 0; i < 5; ++i){
-		queSum[i] = (queSum[i] - p.first <= 0) ? 0 : queSum[i] - p.first ;
-		int this_t = 0;		
-		if(queSum[i] == 0){
-			this_t = p.second;
+	cout  << "== Input : "<< p.first << "  " << p.second << "  " << cnt << "\n";
+	
+	int temp[5];
+	if(prev < p.first){
+		int diff = p.first - prev;
+		for(int i = 0; i < nCPU; ++i){
+			cout << queSum[i] << " -> ";
+			temp[i] = (queSum[i] - diff <= 0) ? 0 : queSum[i] - diff ;
+			cout << queSum[i] << "\n";
 		}
-		else{
-			this_t = queSum[i] + p.second;
-		}
+	}
 
-		if(this_t > limit){
-
+	for(int i =0 ; i < nCPU; ++i){
+		if(temp[i] + p.second > limit) {
+			continue;
 		}
+		queSum[i] = temp[i] + p.second;
+		run(nCPU, p.first, cnt +1);
+		queSum[i] = temp[i] - p.second;
 		
 	}
 
 }
-/*
-void run(int start,  int cnt){
-	if(cnt == n){
-		cout << " All entry is done \n";
-		return;
-	}
-	pair<int,int> p = vp[cnt];
-	int this_t = 0;
-	cout << " Start : " << start << " | cnt " <<  cnt << "\n";
-	cout << " ====> " << p.first << " : " << p.second << " | " << mx << "\n";
-	for(int i = 0; i < 5; ++i){
-		if(i == start )continue;
-		mx = max(mx, i+1);
-		if(queSum[i] == 0){
-			initT[i] = p.first;
-			this_t = p.second;
-			cout << " Init T : " << initT[i] << "\n";
-			cout << " 1 - This T : " << this_t << "\n"; 
-		}
-		else{
-			this_t = queSum[i] + p.second - p.first + initT[i];
-			cout << " 2 - This T : " << this_t << "\n";
-		}
-		if(this_t > limit){
-			cout << " Packet T is large \n";
-			cout << " will go " << i << " " << cnt << "\n";
-			run(i, cnt);
-			return;
-		}
-		else{
-			cout << " Packet T can handled. \n";
-			queSum[i] += p.second;
-			cout << " qumSum[" << i << "] : " << queSum[i] << "\n";
-			cout << " will go " << -1 << " " << cnt+1 << "\n";
-			run(-1, cnt+1);
-			return;
 
-		}
+void proc(){
+	for(int i = 1; i <= 5; ++i){
+		memset(queSum, 0, sizeof(queSum));
+		run(i, 0, 0);
+		if( mx > 0) break;
 	}
-	return;
-
 }
-*/
 
-void packet_proc(){
+
+int main(){
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -94,7 +67,7 @@ void packet_proc(){
 		vp.push_back({tg-bias,tl});
 	}
 
-	run(-1,0);
+	proc();
 
 	cout << mx << "\n";
 
